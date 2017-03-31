354df88596b8a2e04ed3d1aff720136491fe372b
Alexander Wong <admin@alexander-wong.com>
2017-03-30 22:47:32 -0600

    mix phx.new

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -1,51 +1,27 @@
-///////////////////////////////////////////////////////////////////////////////
-// The contents of this file are subject to the Common Public Attribution
-// License Version 1.0. (the "License"); you may not use this file except in
-// compliance with the License. You may obtain a copy of the License at
-// https://raw.githubusercontent.com/udia-software/udia/master/LICENSE.
-// The License is based on the Mozilla Public License Version 1.1, but
-// Sections 14 and 15 have been added to cover use of software over a computer
-// network and provide for limited attribution for the Original Developer.
-// In addition, Exhibit A has been modified to be consistent with Exhibit B.
-//
-// Software distributed under the License is distributed on an "AS IS" basis,
-// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
-// the specific language governing rights and limitations under the License.
-//
-// The Original Code is UDIA.
-//
-// The Original Developer is the Initial Developer.  The Initial Developer of
-// the Original Code is Udia Software Incorporated.
-//
-// All portions of the code written by UDIA are Copyright (c) 2016-2017
-// Udia Software Incorporated. All Rights Reserved.
-///////////////////////////////////////////////////////////////////////////////
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js",
+      joinTo: "js/app.js"
 
       // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
+      // https://github.com/brunch/brunch/blob/master/docs/config.md#files
       // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
+      //  "js/app.js": /^(js)/,
+      //  "js/vendor.js": /^(vendor)|(deps)/
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
-      order: {
-        before: [
-          "vendor/js/jquery-3.1.1.min.js",
-          "vendor/js/semantic.min.js"
-        ]
-      }
+      // https://github.com/brunch/brunch/tree/master/docs#concatenation
+      // order: {
+      //   before: [
+      //     "vendor/js/jquery-2.1.1.js",
+      //     "vendor/js/bootstrap.min.js"
+      //   ]
+      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
-      // order: {
-      //   after: ["assets/css/app.css"] // concat app.css last
-      // }
     },
     templates: {
       joinTo: "js/app.js"
@@ -54,7 +30,7 @@ exports.config = {
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
+    // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(static)/
   },
@@ -62,8 +38,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-      watched: ["static", "css", "js", "vendor"],
-
+    watched: ["static", "css", "js", "vendor"],
     // Where to compile files to
     public: "../priv/static"
   },
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,30 +1,62 @@
-///////////////////////////////////////////////////////////////////////////////
-// The contents of this file are subject to the Common Public Attribution
-// License Version 1.0. (the "License"); you may not use this file except in
-// compliance with the License. You may obtain a copy of the License at
-// https://raw.githubusercontent.com/udia-software/udia/master/LICENSE.
-// The License is based on the Mozilla Public License Version 1.1, but
-// Sections 14 and 15 have been added to cover use of software over a computer
-// network and provide for limited attribution for the Original Developer.
-// In addition, Exhibit A has been modified to be consistent with Exhibit B.
-//
-// Software distributed under the License is distributed on an "AS IS" basis,
-// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
-// the specific language governing rights and limitations under the License.
-//
-// The Original Code is UDIA.
-//
-// The Original Developer is the Initial Developer.  The Initial Developer of
-// the Original Code is Udia Software Incorporated.
-//
-// All portions of the code written by UDIA are Copyright (c) 2016-2017
-// Udia Software Incorporated. All Rights Reserved.
-///////////////////////////////////////////////////////////////////////////////
+// NOTE: The contents of this file will only be executed if
+// you uncomment its entry in "assets/js/app.js".
 
+// To use Phoenix channels, the first step is to import Socket
+// and connect at the socket path in "lib/web/endpoint.ex":
 import {Socket} from "phoenix"
 
-let socket = new Socket("/socket", {
-  params: {token: window.userToken}
-})
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+
+// When you connect, you'll often need to authenticate the client.
+// For example, imagine you have an authentication plug, `MyAuth`,
+// which authenticates the session and assigns a `:current_user`.
+// If the current user exists you can assign the user's token in
+// the connection for use in the layout.
+//
+// In your "lib/web/router.ex":
+//
+//     pipeline :browser do
+//       ...
+//       plug MyAuth
+//       plug :put_user_token
+//     end
+//
+//     defp put_user_token(conn, _) do
+//       if current_user = conn.assigns[:current_user] do
+//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
+//         assign(conn, :user_token, token)
+//       else
+//         conn
+//       end
+//     end
+//
+// Now you need to pass this token to JavaScript. You can do so
+// inside a script tag in "lib/web/templates/layout/app.html.eex":
+//
+//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
+//
+// You will need to verify the user token in the "connect/2" function
+// in "lib/web/channels/user_socket.ex":
+//
+//     def connect(%{"token" => token}, socket) do
+//       # max_age: 1209600 is equivalent to two weeks in seconds
+//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
+//         {:ok, user_id} ->
+//           {:ok, assign(socket, :user, user_id)}
+//         {:error, reason} ->
+//           :error
+//       end
+//     end
+//
+// Finally, pass the token on connect as below. Or remove it
+// from connect if you don't care about authentication.
+
+socket.connect()
+
+// Now that you are connected, you can join channels with a topic:
+let channel = socket.channel("topic:subtopic", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
 
 export default socket
