8bf72d7029373e841527c5138f1be2135f0ebfa9
Alexander Wong <admin@alexander-wong.com>
2017-04-22 17:04:16 -0600

    Added post create test

--- lib/udia/web/controllers/api/post_controller.ex
+++ lib/udia/web/controllers/api/post_controller.ex
@@ -9,13 +9,26 @@ defmodule Udia.Web.Api.PostController do
     render(conn, Udia.Web.PostView, "index.json", posts: posts)
   end
 
-  def create(conn, %{"post" => post_params}) do
+  def create(conn, post_params) do
     user = Guardian.Plug.current_resource(conn)
-    with {:ok, %Post{} = post} <- Logs.create_post(user, post_params) do
+
+    if post_params["title"] == nil or post_params["content"] == nil do
       conn
-      |> put_status(:created)
-      |> put_resp_header("location", post_path(conn, :show, post))
-      |> render(Udia.Web.PostView, "show.json", post: post)
+        |> put_status(:bad_request)
+        |> render(Udia.Web.ErrorView, "error.json", error: "Post must have title and content")
+    else
+      case Logs.create_post(user, post_params) do
+        {:ok, %Post{} = post} ->
+          post = post |> Udia.Repo.preload(:creator)
+          conn
+          |> put_status(:created)
+          |> put_resp_header("location", post_path(conn, :show, post))
+          |> render(Udia.Web.PostView, "show.json", post: post)
+        {:error, msg} ->
+          conn
+          |> put_status(:bad_request)
+          |> render(Udia.Web.ErrorView, "error.json", error: msg)
+      end
     end
   end
 
@@ -33,7 +46,9 @@ defmodule Udia.Web.Api.PostController do
         render(conn, Udia.Web.PostView, "show.json", post: post)
       end
     else
-      {:error, "User does not match post creator."}
+      conn
+      |> put_status(:forbidden)
+      |> render(Udia.Web.ErrorView, "error.json", error: "User does not match post creator.")
     end
   end
 
@@ -46,7 +61,9 @@ defmodule Udia.Web.Api.PostController do
         send_resp(conn, :no_content, "")
       end
     else
-      {:error, "User does not match post creator."}
+      conn
+      |> put_status(:forbidden)
+      |> render(Udia.Web.ErrorView, "error.json", error: "User does not match post creator.")
     end
   end
 end
--- test/web/controllers/api/post_controller_test.exs
+++ test/web/controllers/api/post_controller_test.exs
@@ -4,6 +4,7 @@ defmodule Udia.Web.PostControllerTest do
   alias Udia.Logs
   alias Udia.Logs.Post
 
+  @login_attrs %{username: "udia", password: "hunter2"}
   @create_attrs %{content: "some content", title: "some title"}
   @update_attrs %{content: "some updated content", title: "some updated title"}
   @invalid_attrs %{content: nil, title: nil}
@@ -34,7 +35,37 @@ defmodule Udia.Web.PostControllerTest do
     ]
   end
 
-  # test "creates post and renders post when data is valid"
+  test "creates post and renders post when data is valid", %{conn: conn} do
+    # Login
+    insert_user(@login_attrs)
+    conn = post conn, session_path(conn, :create), @login_attrs
+    assert json_response(conn, 201)
+    jwt = json_response(conn, 201)["token"]
+
+    conn = build_conn()
+    |> put_req_header("authorization", "Bearer: #{jwt}")
+    conn = post conn, post_path(conn, :create), @create_attrs
+    assert json_response(conn, 201)
+  end
+
+
+  test "fails to create post and render post when data is invalid", %{conn: conn} do
+    # Login
+    insert_user(@login_attrs)
+    conn = post conn, session_path(conn, :create), @login_attrs
+    assert json_response(conn, 201)
+    jwt = json_response(conn, 201)["token"]
+
+    conn = build_conn()
+    |> put_req_header("authorization", "Bearer: #{jwt}")
+    conn = post conn, post_path(conn, :create), @invalid_attrs
+    assert %{"error" => "Post must have title and content"} == json_response(conn, 400)
+  end
+
+  test "fails to create post and render post when unauthenticated", %{conn: conn} do
+    conn = post conn, post_path(conn, :create), @create_attrs
+    assert %{"errors" => ["Unauthenticated"]} == json_response(conn, 401)
+  end
 
   # test "does not create post and renders errors when data is invalid"
 
